64aab40956c7132798ae2bd53b2263b0e31d1d9e
Ella Tanttu <ellajtanttu@gmail.com>
2021-12-17 13:29:56 -0800

    commiting before deleting and recloning

---
+++ Tracker/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tracker.Controllers
+{
+  public class HomeController : Controller
+  {
+    
+  }
+}
---
+++ Tracker/Models/Vendor.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+namespace Tracker.Models
+{
+  public class Vendor
+  {
+    public string Name { get; set; } // user input
+    public string Description { get; set; } // user input
+    public int Id { get; } // auto generated
+    // private static List<Vendor> _instances = new List<Vendor> {}; // auto generated
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      // Id = _instances.Count;
+      // _instances.Add(this);
+    }
+  }
+}
